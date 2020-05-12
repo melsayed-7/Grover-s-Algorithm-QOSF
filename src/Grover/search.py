37789015336ff13dd277ceb4a79db2635566547c
Moustafa Elsayed <s-moustafaelsayed@zewailcity.edu.eg>
2020-05-12 23:33:27 +0200

    Refactoring:

--- src/Grover/search.py
+++ src/Grover/search.py
@@ -22,9 +22,7 @@ def search(to_be_searched, target, oracle):
     b=[0,1,2,3,4,5,6,7]
     circuit.x(15)
     circuit.h(a)
-    i=0
-    while i<12:
-        i=i+1
+    for i in range(12):
         #oracle
         circuit = oracle(circuit)
 
