91ccdc94848f98e62132841f4a1ee88c4adba7c4
Moustafa Elsayed <s-moustafaelsayed@zewailcity.edu.eg>
2020-05-27 02:13:55 +0200

    Added gates_count variable to count gates I made all functions take this variable as an argument and whenever a gate is used this variable is increased and at this moment we used 58 gates.

--- src/Grover/oracle.py
+++ src/Grover/oracle.py
@@ -1,6 +1,6 @@
 import qiskit
 
-def oracle(circuit):
+def oracle(circuit, gates_count):
     """[summary]
     This an oracle function that can be later implemented as a class
     It applies specific operations on a given quantum circuit
@@ -8,6 +8,8 @@ def oracle(circuit):
     Arguments:
         circuit {[qiskit.Quantum Circuit]} -- The circuit that we will apply
         the oracle on.
+
+        gates_count {[int]} -- gates_count variable to be updated
 
     Returns:
         [circuit] -- [description]
@@ -18,40 +20,47 @@ def oracle(circuit):
     circuit.barrier()
 
     circuit.x([1,2,5,7])
-    circuit = nbit_toffoli_gate(circuit, 8)
-    circuit.x([1,2,5,7])
+    gates_count += 4  # in a more modular way the count of 0 in the target string
     
+
+    nbit_toffoli_gate(circuit, 8, gates_count)
+    circuit.x([1,2,5,7])
+    gates_count += 4 
     circuit.barrier()
 
-    return circuit
     
 
 
 
-def nbit_toffoli_gate(circuit, N):
+def nbit_toffoli_gate(circuit, N, gates_count):
     """[summary]
     This function applies N-bit toffoli on a given Circuit
 
     Arguments:
-        N {[int]} -- number of qubits used in the toffoli gate
-        circuit {qiskit.Quantum Circuit}] -- The quantum circuit used
+        N {int} -- number of qubits used in the toffoli gate
+        circuit {qiskit.Quantum Circuit} -- The quantum circuit used
+        gates_count {int} -- 
     """
     assert circuit.n_qubits >= 2*N, 'The circuit is not big enough to apply {}-toffoli gate'.format(N)
         
     circuit.ccx(0,1,N)
-    
+    gates_count += 1 
+
     for x in range(2,N):
         y = x+N-2
         z = x+N-1
         circuit.ccx(x,y,z)
+        gates_count += 1
     
     circuit.cx(2*N-2,2*N-1)
+    gates_count += 1
     
     for x in range(7,1,-1):
         y = N+x-2
         z = N+x-1
         circuit.ccx(x,y,z)
+        gates_count += 1
 
     circuit.ccx(0,1,N)
+    gates_count += 1
 
-    return circuit
--- src/Grover/search.py
+++ src/Grover/search.py
@@ -16,24 +16,35 @@ def search(to_be_searched, target, oracle):
     Returns:
         [int] -- position of the target key in the array
     """
+
+    gates_count = 0
     #string to be found is 01011001
     circuit=QuantumCircuit(16,8)
     a=[0,1,2,3,4,5,6,7,15]
     b=[0,1,2,3,4,5,6,7]
+    
     circuit.x(15)
+    gates_count += 1
+
     circuit.h(a)
+    gates_count += len(a)
+
     for i in range(12):
-        
         #oracle
-        circuit = oracle(circuit)
+        # circuit = 
+        oracle(circuit, gates_count)
 
         #grover diffusion operator
         circuit.barrier()
         circuit.h(b)
         circuit.x(b)
-        circuit = nbit_toffoli_gate(circuit, 8)
+        gates_count += 2
+
+        nbit_toffoli_gate(circuit, 8, gates_count)
+        
         circuit.x(b)
         circuit.h(b)
+        gates_count += 2
 
 
     circuit.barrier()
@@ -43,6 +54,7 @@ def search(to_be_searched, target, oracle):
     result=execute(circuit,simulator).result()
     measurement=result.get_counts()
     print(measurement)
+    print(gates_count)
 
     return measurement
 
